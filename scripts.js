89c53efc97ba5a0e9c2ed90106eda0362eb96459
-THE-LAST-SAMURAI- <connor813@outlook.com>
2021-08-09 10:57:30 -0500

    added home, about and contact page

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function AboutPage() {
+    document.getElementById("PageContent").innerHTML = "naturally made ice cream since 1807";
+}
+
+function ContactPage() {
+    document.getElementById("PageContent").innerHTML = "this is the contact page";
+}
+
+function MenuPage() {
+    document.getElementById("PageContent").innerHTML = "this is the menu page";
+}
+
